58e8642da4d8a37b7e1dff4bef4fca8192489569
Yosed Camilo Sanchez P. <Camilos0704@gmail.com>
2026-02-27 22:06:33 -0500

    avances

--- Intro node/CursoExpress/app.js
+++ Intro node/CursoExpress/app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 
 const app = express();
-const infoCursos = require('./cursos.js')
+const infoCursos = require('./datos/cursos.js')
 
 //Routers
-const routerProgramacion = express.Router();
+const routerProgramacion = require('./routers/programacion.js')
 app.use('/api/cursos/programacion', routerProgramacion);
 
+const routerMatematicas = require('./routers/matematicas.js')
+app.use('/api/cursos/matematicas',routerMatematicas );
 // Routing
 app.get('/', (req,res)=>{
     res.send('Mi primer servidor. Cursos')
@@ -14,48 +16,6 @@ app.get('/', (req,res)=>{
 
 app.get('/api/cursos',(req,res)=>{
     res.send(JSON.stringify(infoCursos))
-})
-
-app.get('/api/cursos/programacion', (req,res)=>{
-    res.send(JSON.stringify(infoCursos.programacion))
-})
-app.get('/api/cursos/matematicas', (req,res)=>{
-    res.send(JSON.stringify(infoCursos.matematicas))
-})
-app.get('/api/cursos/programacion/:lenguaje',(req,res)=>{
-    const lenguaje = req.params.lenguaje;
-    const resultados = infoCursos.programacion.filter(cursos=>cursos.lenguaje === lenguaje)
-
-    if(resultados.length===0){
-        return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
-    }
-    res.send(JSON.stringify(resultados))
-
-    if(req.query.ordenar === 'vistas'){
-        res.send(JSON.stringify(resultados.sort((a,b)=>b.vistas - a.vistas)))
-    }else{
-        res.send(JSON.stringify(resultados))
-    }
-})
-app.get('/api/cursos/matematicas/:tema', (req,res)=>{
-    const tema= req.params.tema;
-    const resultados = infoCursos.matematicas.filter(cursos =>cursos.tema === tema)
-
-    if(resultados.length ===0){
-        return res.status(404).send(`No se encontraron cursos de ${tema}`)
-    }
-    res.send(JSON.stringify(resultados))
-})
-app.get('/api/cursos/programacion/:lenguaje/:nivel',(req,res)=>{
-    const lenguaje = req.params.lenguaje
-    const nivel = req.params.nivel
-
-    const resultados = infoCursos.programacion.filter(curso =>curso.lenguaje===lenguaje && curso.nivel ===nivel)
-
-    if(resultados.length===0){
-        return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`)
-    }
-    res.send(JSON.stringify(resultados))
 })
 
 const PUERTO = process.env.PORT || 3000;
---
+++ Intro node/CursoExpress/routers/programacion.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const routerProgramacion = express.Router();
+
+const {programacion} = require('../datos/cursos');
+
+routerProgramacion.get('/', (req,res)=>{
+    res.send(JSON.stringify(programacion))
+})
+//Middleware
+routerProgramacion.use(express.json())
+
+routerProgramacion.get('/:lenguaje',(req,res)=>{
+    const lenguaje = req.params.lenguaje;
+    const resultados = programacion.filter(cursos=>cursos.lenguaje === lenguaje)
+
+    if(resultados.length===0){
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
+    }
+    res.send(JSON.stringify(resultados))
+
+    if(req.query.ordenar === 'vistas'){
+        res.send(JSON.stringify(resultados.sort((a,b)=>b.vistas - a.vistas)))
+    }else{
+        res.send(JSON.stringify(resultados))
+    }
+})
+routerProgramacion.get('/:lenguaje/:nivel',(req,res)=>{
+    const lenguaje = req.params.lenguaje
+    const nivel = req.params.nivel
+
+    const resultados = programacion.filter(curso =>curso.lenguaje===lenguaje && curso.nivel ===nivel)
+
+    if(resultados.length===0){
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`)
+    }
+    res.send(JSON.stringify(resultados))
+})
+routerProgramacion.post('/', (req,res)=>{
+    let cursoNuevo = req.body;
+    programacion.push(cursoNuevo);
+    res.send(JSON.stringify(programacion))
+});
+routerProgramacion.put('/:id',(req,res)=>{
+    const cursoAtualizado =req.body;
+    const id = req.params.id;
+    const indice = programacion.findIndex(curso => curso.id ==id);
+    if(indice){
+        programacion[indice]=cursoAtualizado;
+    }
+    res.send(JSON.stringify(programacion))
+});
+routerProgramacion.patch('/:id',(req,res)=>{
+    const infoActualizada = req.body;
+    const id = req.params.id;
+
+    const indice = programacion.findIndex(curso => curso.id == id)
+    if(indice >= 0){
+        const cursoAModificar = programacion[indice]
+        Object.assign(cursoAModificar,infoActualizada)
+    }
+    res.send(JSON.stringify(programacion))
+})
+module.exports = routerProgramacion 
