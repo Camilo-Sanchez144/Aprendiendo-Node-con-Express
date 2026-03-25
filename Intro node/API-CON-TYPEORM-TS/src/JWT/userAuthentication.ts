8b45dc697aba12ad41b0e5a10320bf5c200509c6
Yosed Camilo Sanchez P. <Camilos0704@gmail.com>
2026-03-25 09:20:40 -0500

    update

--- Intro node/API-CON-TYPEORM-TS/src/JWT/userAuthentication.ts
+++ Intro node/API-CON-TYPEORM-TS/src/JWT/userAuthentication.ts
@@ -7,23 +7,18 @@ export const authMiddleware = (
     next: NextFunction
 ) => {
 
-    // Solución para manejar el caso en que 'authHeader' sea undefined
     const authHeader = req.headers['authorization'];
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({ message: 'No autorizado' });
     }
-
-    // Solución para asegurar que 'token' no sea undefined
     const token = authHeader.split(' ')[1];
     if (!token) {
         return res.status(401).json({ message: 'Token no proporcionado' });
     }
 
     try {
-        // Solución para asegurar que 'token' sea un string válido
         const decoded = jwt.verify(token, 'secret') as { [key: string]: any };
 
-        // Solución para extender el tipo de 'req' y agregar 'user'
         (req as any).user = decoded;
         next();
 
